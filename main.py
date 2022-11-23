b276ce3fb03487c8d6ca1fd5b7a731234ec0912e
j0pl0p <matveysokolov514@gmail.com>
2022-11-23 20:35:12 +0300

    123

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+
+        self.btn.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+
+    def draw_circle(self, qp):
+        qp.setPen(QColor(255, 255, 0))
+        x, y = random.randint(30, 400), random.randint(30, 300)
+        r = min(random.randint(15, 420-x), random.randint(15, 320-y))
+        qp.drawEllipse(x, y, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
