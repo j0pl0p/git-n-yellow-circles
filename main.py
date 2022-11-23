12a8c6bb28a9079604c11bcb01e8b1156c868463
j0pl0p <matveysokolov514@gmail.com>
2022-11-23 20:51:32 +0300

    git and random circles

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QPainter, QColor
 
+from UI import Ui_MainWindow
 
-class MyWidget(QMainWindow):
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
 
         self.btn.clicked.connect(self.paint)
@@ -25,7 +27,7 @@ class MyWidget(QMainWindow):
             self.draw_circle(qp)
 
     def draw_circle(self, qp):
-        qp.setPen(QColor(255, 255, 0))
+        qp.setPen(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         x, y = random.randint(30, 400), random.randint(30, 300)
         r = min(random.randint(15, 420-x), random.randint(15, 320-y))
         qp.drawEllipse(x, y, r, r)
